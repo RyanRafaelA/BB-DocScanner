b10d30de0906d2109b15c5590686d4cd50ff9066
RyanRafaelA <ryrafael461@gmail.com>
2024-09-19 21:28:53 -0300

    Creating the backEnd directory

---
+++ backEnd/README.md
@@ -0,0 +1,12 @@
+# BB-DocScanner
+
+### Requisitos
+pip install fastapi uvicorn
+
+pip install pytesseract
+
+pip install opencv-python
+
+pip install pdf2image
+
+pip install PyMuPDF
--- backEnd/main.py
+++ backEnd/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse, HTMLResponse, FileResponse
-from PIL import Image
 import pytesseract
-import io
 from pathlib import Path
 import cv2
 import os
+
+pytesseract.pytesseract.tesseract_cmd = "C:\\py-libs\\Tesseract-OCR\\Tesseract.exe"
 
 app = FastAPI()
 
@@ -19,7 +19,8 @@ Path(UPLOAD_FOLDER).mkdir(parents=True, exist_ok=True)
 @app.get("/", response_class=HTMLResponse)
 def upload_form():
     try:
-        with open("frontEnd/index.html") as file:  # Página de onde puxamos os arquivos
+        # Página de onde puxamos os arquivos
+        with open("C:\\Users\\Public\\faculdade\\residencia4\\BB-DocScanner\\frontEnd\\index.html") as file:  
             return file.read()
     except FileNotFoundError:
         return JSONResponse(content={"error": "HTML file not found"}, status_code=404)
@@ -49,7 +50,7 @@ async def create_upload_file(file: UploadFile = File(...)):
 
         # Aplicar OCR usando pytesseract
         text = pytesseract.image_to_string(image, config="--psm 6")
-
+        
         return JSONResponse(content={"text": text})
     else:
         return JSONResponse(content={"error": "Unsupported file type"}, status_code=400)
