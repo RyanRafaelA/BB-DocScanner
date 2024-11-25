2a6117c1a4675be0b00a2751573ad4f9de2ad7a5
RyanRafaelA <ryrafael461@gmail.com>
2024-11-25 10:13:09 -0300

    Improved Post endpoint and new readme

--- src/controller/controllers.py
+++ src/controller/controllers.py
@@ -16,14 +16,17 @@ app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
-@api.route('/scan')
+@api.route('/scan', endpoint='scanner')
 class Scan(Resource):
     def post(self):
         if 'file' not in request.files or 'message' not in request.form:
-            return 'Nenhum arquivo ou mensagem fornecido.', 400
+            return 'Nenhum chave file ou message foi fornecido.', 400
 
         file = request.files['file']
         message = request.form['message']
+        
+        if message == '' or file.filename == '':
+            return 'Nenhuma mensagem e arquivo foi enviada.', 400
 
         if file:
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.name)
@@ -31,7 +34,10 @@ class Scan(Resource):
 
             # Realizar OCR no arquivo
             extracted_text = process_file(file_path)
-
+            
+            if extracted_text == '':
+                return 'Arquivo em branco.', 400
+            
             # Filtrar a informação baseada na mensagem do usuário
             requested_info = search_information(extracted_text, message)
 
