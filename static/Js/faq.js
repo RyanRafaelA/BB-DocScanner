4ee2a22ab18ed056afe32a9cc16aca594e73be72
Gledson <gledsonst366@gmail.com>
2024-11-24 21:05:47 -0300

    tela de ajuda 90% implementado

--- static/Js/faq.js
+++ static/Js/faq.js
@@ -14,10 +14,8 @@ faqItems.forEach(item => {
         // Verifica se a resposta está visível e ajusta a exibição
         if (item.classList.contains('active')) {
             answer.style.display = 'block';
-            arrow.textContent = '▲'; // Muda a seta para cima
         } else {
             answer.style.display = 'none';
-            arrow.textContent = '▼'; // Muda a seta para baixo
         }
     });
 });
